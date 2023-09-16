f69862330f137137f1b95d445bf79a69541e084f
vinsmokezoro <145193274+vinsmokezoro@users.noreply.github.com>
2023-09-17 00:54:41 +0530

    3 files modified

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FootballService } from 'src/app/services/football.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,17 +8,9 @@ import { FootballService } from 'src/app/services/football.service';
   host: { 'collision-id': 'DashboardComponent' },
 })
 export class DashboardComponent implements OnInit {
-
   countries: string[] = ['England', 'Spain', 'France', 'Germany', 'Italy'];
 
-  constructor(private router: Router,
-    private footballService: FootballService) {}
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
-
-  routeToStandings(country: string) {
-    this.footballService.selectedCountry = country;
-    this.router.navigate(['/dashboard/standings']);
-  }
+  ngOnInit(): void {}
 }
